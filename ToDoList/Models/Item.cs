4a7ed12a19f0752c0651ba5009fcebe6ded75c9d
Betty Mutai <bettymutai00@gmail.com>
2022-04-22 15:09:50 +0300

    Add priority property as an overloaded constructor.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -3,12 +3,19 @@
     public class Item
     {
         public string Description { get; set; }
+        public int Priority { get; set; }
         private static readonly List<Item> _instances = new() { };
 
         public Item(string description)
         {
             Description = description;
             _instances.Add(this);
+        }
+
+        public Item(string description, int priority)
+            :this(description)
+        {
+            Priority = priority;
         }
 
         public static void ClearAll()
