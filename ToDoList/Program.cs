ac5638047a53977f4bc5ef0ae866ec48ee3a4239
Betty Mutai <bettymutai00@gmail.com>
2022-04-27 11:48:52 +0300

    Add ASP.NET framework reference and configure code.

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ToDoList
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace ToDoList
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ToDoList
 {
-    internal class Startup
+    public class Startup
     {
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseRouting();
+            app.UseEndpoints(routes =>
+            {
+                routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
     }
 }
