acb3e61434e1ebf0c2a7064d04cf74df0999ee1c
Betty Mutai <bettymutai00@gmail.com>
2022-04-28 12:39:50 +0300

    Add link to description and route to show item details.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -31,5 +31,12 @@ namespace ToDoList.Controllers
             Item.ClearAll();
             return View();
         }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Show(int id)
+        {
+            Item foundItem = Item.Find(id);
+            return View(foundItem);
+        }
     }
 }
