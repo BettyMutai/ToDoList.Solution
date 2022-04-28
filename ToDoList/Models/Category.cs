27eb30e78ab7caed66c1fdf83bd10fea6f2c2085
Betty Mutai <bettymutai00@gmail.com>
2022-04-28 14:47:27 +0300

    Add test and logic to find category by Id.

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -25,5 +25,10 @@
         {
             return _instances;
         }
+
+        public static Category Find(int searchId)
+        {
+            return _instances[searchId - 1];
+        }
     }
 }
