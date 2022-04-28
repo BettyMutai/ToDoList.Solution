b84c370bc43269952576d94d70a7fbe4d6364daa
Betty Mutai <bettymutai00@gmail.com>
2022-04-28 13:13:31 +0300

    Add test to confirm constructor can successfully create Category objects and add constructor.

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
 
 namespace ToDoList.Tests.ModelTests
 {
-    internal class CategoryTests
+    [TestClass]
+    public class CategoryTests
     {
+        [TestMethod]
+        public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+        {
+            Category newCategory = new Category("test category");
+            Assert.AreEqual(typeof(Category), newCategory.GetType());
+        }
     }
 }
--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -3,8 +3,16 @@
     public class Category
     {
         private static List<Category> _instances = new List<Category> { };
+
         public string Name { get; set; }
         public int Id { get; }
         public List<Item> Items { get; set; }
+        public Category(string categoryName)
+        {
+            Name = categoryName;
+            _instances.Add(this);
+            Id = _instances.Count;
+            Items = new List<Item> { };
+        }
     }
 }
